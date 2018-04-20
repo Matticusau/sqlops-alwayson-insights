99c333f1fc46922bf775139b46061c8f8bb04653
Matt Lavery <mlavery@microsoft.com>
2018-04-20 12:54:13 +1000

    Initial migration of code

---
+++ src/sql/alwayson-ag-replica-health-detail.sql
@@ -0,0 +1,15 @@
+-- 
+-- Author: Matticusau
+-- Purpose: Provides detailed data for the Replica Health Insights Widget
+-- License: https://github.com/Matticusau/sqlops-widgets/blob/master/LICENSE
+-- 
+SELECT ag.name [ag_name]
+    , ar.replica_server_name
+    , ars.role_desc
+    , ars.operational_state
+    , ars.connected_state_desc
+    , ars.synchronization_health_desc
+FROM sys.availability_groups ag
+INNER JOIN sys.dm_hadr_availability_replica_states ars ON ars.group_id = ag.group_id
+INNER JOIN sys.availability_replicas ar ON ar.replica_id = ars.replica_id
+ORDER BY ag_name, replica_server_name
